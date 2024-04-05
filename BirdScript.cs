758ea06e240a5fc0663da76f038f2c0f1d1d015d
Hamza Raza <126276208+mohamzaraza@users.noreply.github.com>
2024-04-04 22:41:39 -0400

    Add files via upload

---
+++ BirdScript.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+    public Rigidbody2D myRigidbody;
+    public float flapStrength;
+    public LogicScript logic;
+    public bool birdisAlive = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && birdisAlive)
+        {
+            myRigidbody.velocity = Vector2.up * flapStrength;
+
+        }
+        myRigidbody.gravityScale = 5;
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        birdisAlive = false;
+    }
+}
